7552001a505ccc28df20a9c94ca0c208f720f71f
Brian Hague <bhague1281@gmail.com>
2026-03-30 20:29:02 -0700

    week1 lecture code + template

---
+++ week1/warmup.js
@@ -0,0 +1,12 @@
+function deleteById(items, id) {
+  // Given items is an array of objects with a
+  // property “id”, delete the object with the id
+  // provided and return the updated array of items
+}
+
+function updateById(items, id, newData) {
+  // Given items is an array of objects with a
+  // property “id”, update the object with the id
+  // and data provided, and return the updated
+  // array of items
+}
