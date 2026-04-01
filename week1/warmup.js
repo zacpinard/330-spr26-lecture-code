6ac9c4622f26d4e0816597720a64c064cb360cbd
zacpinard <zacpinard@gmail.com>
2026-03-31 19:09:25 -0700

    warmup

--- week1/warmup.js
+++ week1/warmup.js
@@ -2,11 +2,39 @@ function deleteById(items, id) {
   // Given items is an array of objects with a
   // property “id”, delete the object with the id
   // provided and return the updated array of items
+
+  // return items.filter((item) => item.id !==id);
+  const index = items.findIndex((item) => item.id === id);
+  items.splice(index, 1)
+  return items
+
 }
+
+const someItems = [
+  {id: '1', name: 'Zac'},
+  {id: '2', name: 'James'}
+
+]
 
 function updateById(items, id, newData) {
   // Given items is an array of objects with a
   // property “id”, update the object with the id
   // and data provided, and return the updated
   // array of items
+
+  return items.map((item) => {
+    if (item.id !== id) {
+      return item
+    }
+
+    return {
+      id,
+      ...newData
+    }
+  })
+
 }
+
+// console.log(updateById(someItems, '1', {name: 'Champ'}))
+console.log(deleteById(someItems, '2'))
+
